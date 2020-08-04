53efcfc599a51ed636bc4fdb6153062d3adfe608
samuelematias <hi@samuelematias.com>
2020-08-04 16:34:12 -0300

    feat(screen): create AboutView

---
+++ lib/views/about/about_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AboutView extends StatelessWidget {
+  const AboutView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
