262bde0001fe4e4a1029bd1147d4ed36c5b29848
samuelematias <hi@samuelematias.com>
2020-08-04 16:33:38 -0300

    feat(widgets): create LayoutTemplate widget

---
+++ lib/views/layout_template/layout_template.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:the_basics/locator.dart';
+import 'package:the_basics/routing/route_names.dart';
+import 'package:the_basics/routing/router.dart';
+import 'package:the_basics/services/navigation_service.dart';
+import 'package:the_basics/widgets/centered_view/centered_view.dart';
+import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
+import 'package:the_basics/widgets/navigation_drawer/navigation_drawer.dart';
+
+class LayoutTemplate extends StatelessWidget {
+  const LayoutTemplate({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        key: _scaffoldKey,
+        drawer: sizingInformation.isMobile ? NavigationDrawer() : null,
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: <Widget>[
+              NavigationBar(
+                scaffoldKey: _scaffoldKey,
+              ),
+              Expanded(
+                child: Navigator(
+                  key: locator<NavigationService>().navigatorKey,
+                  onGenerateRoute: generateRoute,
+                  initialRoute: HomeRoute,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
