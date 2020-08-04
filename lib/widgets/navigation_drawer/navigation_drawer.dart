ccffbe2c995bdc462d8c33d043bbc142214a9976
samuelematias <hi@samuelematias.com>
2020-08-04 16:35:39 -0300

    feat(widgets): update widgets

--- lib/widgets/navigation_drawer/navigation_drawer.dart
+++ lib/widgets/navigation_drawer/navigation_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_basics/routing/route_names.dart';
 import 'package:the_basics/widgets/navigation_drawer/drawer_item.dart';
 import 'package:the_basics/widgets/navigation_drawer/navigation_drawer_header.dart';
 
@@ -15,8 +16,8 @@ class NavigationDrawer extends StatelessWidget {
       child: Column(
         children: <Widget>[
           NavigationDrawerHeader(),
-          DrawerItem('Episodes', Icons.videocam),
-          DrawerItem('About', Icons.help),
+          DrawerItem('Episodes', Icons.videocam, EpisodesRoute),
+          DrawerItem('About', Icons.help, AboutRoute),
         ],
       ),
     );
