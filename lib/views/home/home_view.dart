4f537a7beb093bb14551f23c81c54ffa8cf3754f
samuelematias <hi@samuelematias.com>
2020-08-04 12:41:20 -0300

    feat(widget): create navigation_drawer widget

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -4,23 +4,29 @@ import 'package:the_basics/views/home/home_content_desktop.dart';
 import 'package:the_basics/views/home/home_content_mobile.dart';
 import 'package:the_basics/widgets/centered_view/centered_view.dart';
 import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
+import 'package:the_basics/widgets/navigation_drawer/navigation_drawer.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CenteredView(
-        child: Column(
-          children: <Widget>[
-            NavigationBar(),
-            Expanded(
-              child: ScreenTypeLayout(
-                mobile: HomeContentMobile(),
-                desktop: HomeContentDesktop(),
-              ),
-            )
-          ],
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? NavigationDrawer()
+            : null,
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: <Widget>[
+              NavigationBar(),
+              Expanded(
+                child: ScreenTypeLayout(
+                  mobile: HomeContentMobile(),
+                  desktop: HomeContentDesktop(),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
