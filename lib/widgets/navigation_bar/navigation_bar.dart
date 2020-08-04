24812b2fadd2d486aa6918e60a11cfd99657893a
samuelematias <hi@samuelematias.com>
2020-08-04 12:04:56 -0300

    feat(widget): create navBar mobile/tablet/desktop

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:the_basics/widgets/navigation_bar/navbar_item.dart';
-import 'package:the_basics/widgets/navigation_bar/navbar_logo.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:the_basics/widgets/navigation_bar/navigation_bar_mobile.dart';
+import 'package:the_basics/widgets/navigation_bar/navigation_bar_tablet_desktop.dart';
 
 class NavigationBar extends StatelessWidget {
+  const NavigationBar({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          NavBarLogo(),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              NavBarItem('Episodes'),
-              SizedBox(
-                width: 60,
-              ),
-              NavBarItem('About'),
-            ],
-          )
-        ],
-      ),
+    return ScreenTypeLayout(
+      mobile: NavigationBarMobile(),
+      tablet: NavigationBarTabletDesktop(),
     );
   }
 }
