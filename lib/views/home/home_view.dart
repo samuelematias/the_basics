ac0f2fcb5ed41de22c4e7c7328157575aaae54c2
samuelematias <hi@samuelematias.com>
2020-08-04 12:12:29 -0300

    feat(widget): create home_content mobile/desktop

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:the_basics/widgets/call_to_action/call_to_action.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:the_basics/views/home/home_content_desktop.dart';
+import 'package:the_basics/views/home/home_content_mobile.dart';
 import 'package:the_basics/widgets/centered_view/centered_view.dart';
-import 'package:the_basics/widgets/course_details/course_details.dart';
 import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +15,10 @@ class HomeView extends StatelessWidget {
           children: <Widget>[
             NavigationBar(),
             Expanded(
-              child: Row(children: [
-                CourseDetails(),
-                Expanded(
-                  child: Center(
-                    child: CallToAction('Join Course'),
-                  ),
-                )
-              ]),
+              child: ScreenTypeLayout(
+                mobile: HomeContentMobile(),
+                desktop: HomeContentDesktop(),
+              ),
             )
           ],
         ),
