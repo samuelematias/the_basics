2e3357a409766270b7012f2b07bee8cef108c6a6
samuelematias <hi@samuelematias.com>
2020-08-04 13:04:22 -0300

    feat(drawer): implemented click onDrawer

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -6,11 +6,19 @@ import 'package:the_basics/widgets/centered_view/centered_view.dart';
 import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
 import 'package:the_basics/widgets/navigation_drawer/navigation_drawer.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) => Scaffold(
+        key: _scaffoldKey,
         drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
             ? NavigationDrawer()
             : null,
@@ -18,7 +26,9 @@ class HomeView extends StatelessWidget {
         body: CenteredView(
           child: Column(
             children: <Widget>[
-              NavigationBar(),
+              NavigationBar(
+                scaffoldKey: _scaffoldKey,
+              ),
               Expanded(
                 child: ScreenTypeLayout(
                   mobile: HomeContentMobile(),
--- lib/widgets/navigation_bar/navigation_bar_mobile.dart
+++ lib/widgets/navigation_bar/navigation_bar_mobile.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:the_basics/widgets/navigation_bar/navbar_logo.dart';
 
 class NavigationBarMobile extends StatelessWidget {
-  const NavigationBarMobile({Key key}) : super(key: key);
+  const NavigationBarMobile({Key key, this.scaffoldKey}) : super(key: key);
+
+  final GlobalKey<ScaffoldState> scaffoldKey;
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +15,10 @@ class NavigationBarMobile extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                scaffoldKey?.currentState?.openDrawer();
+              }),
           NavBarLogo()
         ],
       ),
