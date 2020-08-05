a3fa01114799e3ed2ebfc2db3f9e62eac26fcfa4
samuelematias <hi@samuelematias.com>
2020-08-05 10:26:38 -0300

    feat(project): update files

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -7,16 +7,32 @@ class Api {
   static const String _apiEndpoint =
       'https://us-central1-thebasics-2f123.cloudfunctions.net/thebasics';
 
-  Future getEpisodes() async {
+  Future<dynamic> getEpisodes() async {
     var response = await http.get('$_apiEndpoint/courseEpisodes');
 
     if (response.statusCode == 200) {
       var episodes = (json.decode(response.body) as List)
-          .map((e) => EpisodeItemModel.fromJson(e))
+          .map((episode) => EpisodeItemModel.fromJson(episode))
           .toList();
       return episodes;
     }
 
-    return 'Could not fetch episodes at this time';
+    // something wrong happened
+    return 'Could not fetch the episodes at this time';
+  }
+
+  Future<dynamic> getEpisode(int id) async {
+    var response = await http.get('$_apiEndpoint/episode?id=$id');
+
+    print(
+        'getEpisode | response: ${response.body} statusCode: ${response.statusCode}');
+
+    if (response.statusCode == 200) {
+      var episode = EpisodeItemModel.fromJson(json.decode(response.body));
+      return episode;
+    }
+
+    // something wrong happened
+    return 'Could not fetch episode $id. Make sure it exists';
   }
 }
