5c81b5d643184a164396e9dde89f5b911deef276
samuelematias <hi@samuelematias.com>
2020-08-05 11:48:22 -0300

    feat(screen): implemented open detail screen

--- lib/viewmodels/episode_list_view_model.dart
+++ lib/viewmodels/episode_list_view_model.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:the_basics/locator.dart';
+import 'package:the_basics/routing/route_names.dart';
+import 'package:the_basics/services/navigation_service.dart';
 
-class EpisodeListViewModel extends ChangeNotifier {}
+class EpisodeListViewModel extends ChangeNotifier {
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  void navigateToEpisode(int index) {
+    _navigationService
+        .navigateTo(EpisodeDetailsRoute, queryParams: {'id': index.toString()});
+  }
+}
