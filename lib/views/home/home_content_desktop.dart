353fccf7c912370ec5243d28cbd68a425b01ff09
samuelematias <hi@samuelematias.com>
2020-08-05 13:31:16 -0300

    feat(ui): fix responsive layout

--- lib/views/home/home_content_desktop.dart
+++ lib/views/home/home_content_desktop.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:the_basics/widgets/call_to_action/call_to_action.dart';
 import 'package:the_basics/widgets/course_details/course_details.dart';
+import 'package:the_basics/widgets/list_view_column/list_view_column.dart';
 
 class HomeContentDesktop extends StatelessWidget {
   const HomeContentDesktop({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        CourseDetails(),
-        Expanded(
-          child: Center(
-            child: CallToAction('Join Course'),
-          ),
-        )
+    return ListViewColumn(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: <Widget>[
+            CourseDetails(),
+            Expanded(
+              child: Center(
+                child: CallToAction('Join Course'),
+              ),
+            )
+          ],
+        ),
       ],
     );
   }
